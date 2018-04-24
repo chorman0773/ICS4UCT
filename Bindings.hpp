ee055b2991814543ad4176ea139732fd4bfc0b9b
chorman0773 <chorman64@gmail.com>
2018-04-24 09:39:03 -0400

    Update Bindings.hpp to include new User class

--- Bindings.hpp
+++ Bindings.hpp
@@ -1,5 +1,21 @@
 #ifndef __Bindings_hpp_2018_04_24_09_28
 #define __Bindings_hpp_2018_04_24_09_28
+#include <string>
+#include "Hash.hpp"
+#include "UUID.hpp"
+
+using std::string;
+
+class User : public Hashable{
+	string name;
+	UUID id;
+	double pay;
+	unsigned char publicKey[128];
+public:
+	User();
+	User newUser(const string&);
+	int32_t hashCode()const;
+};
 
 
 #endif
